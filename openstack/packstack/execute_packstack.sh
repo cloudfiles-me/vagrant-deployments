60c547be4b04d725e9c4570cc6d20d39173c3882
jorgeluishdez <swatcat@gmail.com>
2017-06-13 10:57:33 -0500

    Added single node all in deployment

--- openstack/packstack/execute_packstack.sh
+++ openstack/packstack/execute_packstack.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-sudo /usr/bin/packstack --answer-file /vagrant/packstack/answer.ini &> /vagrant/packstack/packstack.log
+deployment_type="$1"
+
+case $deployment_type in
+    -a|allin)
+      sudo /usr/bin/packstack --answer-file /vagrant/packstack/answer_allin.ini &> /vagrant/packstack/packstack.log
+      # echo "ALL IN"
+    ;;
+    -d|distributed)
+      sudo /usr/bin/packstack --answer-file /vagrant/packstack/answer.ini &> /vagrant/packstack/packstack.log
+      # echo "DISTRIBUTED"
+    ;;
+    *)
+      sudo /usr/bin/packstack --answer-file /vagrant/packstack/answer_allin.ini &> /vagrant/packstack/packstack.log
+      # echo "DEFAULT"
+    ;;
+esac
+
 exit
